f266d81efaaa02d8bdb701a495ecc32aa7f69e52
GreenMeansGoMM <47602992+GreenMeansGoMM@users.noreply.github.com>
2022-01-24 17:32:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -9,16 +9,31 @@ var bodyparser = require('body-parser');
 app.use(express.json());
 require('dotenv').config()
 app.use('/', routes);
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/index.html');
+});
 app.use(helmet());
 
 mongoose.connect(
     process.env.MONGODB_URI,
-    
-    (err) =>{
-        if (err) return console.log("Error: ", err);
-        console.log("MongoDB Connection --- Ready state is:", mongoose.connection.readyState);
+    {
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+      replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    },
+    function (err) {
+      if (err) return console.log("Error: ", err);
+      console.log(
+        "MongoDB Connection -- Ready state is:",
+        mongoose.connection.readyState
+      );
     }
-)
+  );
+  
 app.use(bodyparser.json);
 const listener = app.listen(process.env.PORT || 5000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
